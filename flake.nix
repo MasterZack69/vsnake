2611fff0532cfc20b271d21f878906743620c1ac
MasterZack69 <zackdevil2000@gmail.com>
2026-06-01 12:02:49 +0530

    shell is legacy

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              clang
+              clang-tools
+              alsa-lib
+            ];
+          };
+        }
+      );
+    };
+}
