445b38b31a65f6f21071921991a84ce4a55025d2
MasterZack69 <zackdevil2000@gmail.com>
2026-07-30 14:35:14 +0530

    nix packaging

--- flake.nix
+++ flake.nix
@@ -5,11 +5,70 @@
   };
 
   outputs =
-    { nixpkgs, systems, ... }:
+    { self, nixpkgs, systems, ... }:
     let
       eachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "vsnake";
+            version = "0.1.0";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [ makeWrapper ];
+
+            buildPhase = ''
+              runHook preBuild
+              $CXX -std=c++11 -O2 snake.cpp -o vsnake -lm
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              mkdir -p $out/bin
+              cp vsnake $out/bin/
+              runHook postInstall
+            '';
+
+            meta = {
+              description = "A terminal-based snake game with sound effects";
+              homepage = "https://github.com/MasterZack69/vsnake";
+              license = pkgs.lib.licenses.agpl3Only;
+              mainProgram = "vsnake";
+              platforms = pkgs.lib.platforms.linux;
+            };
+          };
+        }
+      );
+
+      checks = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = self.packages.${system}.default;
+        }
+      );
+
+      apps = eachSystem (
+        system:
+        {
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/vsnake";
+          };
+        }
+      );
+
+      formatter = eachSystem (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+
       devShells = eachSystem (
         system:
         let
@@ -18,13 +77,10 @@
         {
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
-              clang
+              gcc
               clang-tools
-              alsa-lib
+              alsa-utils
             ];
-            shellHook = ''
-              echo "Shell Loaded"
-            '';
           };
         }
       );
